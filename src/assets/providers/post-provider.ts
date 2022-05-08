baa6d4169a9890d6eb2b9979239e5359fe9b057c
Átila Magalhães <atilamagalhaes4@gmail.com>
2022-05-08 00:01:32 -0300

    Adicionamento de footer
    Adicionei um footer as paginas.

--- src/assets/providers/post-provider.ts
+++ src/assets/providers/post-provider.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class PostProvider{
-    server: string = "https://www.suaUrl.com.br/assets/api";
+  server: string = "https://www.maragojipe.ba.gov.br/assets/api";
+//    server: string = "https://www.suaUrl.com.br/assets/api";
   constructor(
       private http : HttpClient
     ){  } 
